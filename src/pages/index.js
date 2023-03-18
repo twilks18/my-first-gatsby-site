0219edec6b421eb54825d2abbf6bdef26b067cae
TaNeisha Wilks <twilks18@gmail.com>
2023-03-17 20:42:48 -0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 
 
@@ -7,6 +7,9 @@ const IndexPage = () => {
   return (
       <Layout pageTitle={"Home Page"}>
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+      alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+      src="../images/clifford.jpg"></StaticImage>
     </Layout>
   )
 }
