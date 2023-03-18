31313ea32dcb957baf479867afd0d30b6da9480c
TaNeisha Wilks <twilks18@gmail.com>
2023-03-17 20:15:01 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle={"About Me"}>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
+    )
+}
+
+
+export const Head = () => { 
+    return (
+        <>
+        <title>About Me</title>
+        <meta name='description' content='Your description'></meta>
+        </>
+    
+    )
+}
+
+export default AboutPage
